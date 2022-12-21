b1e1cb03f94ba2bdca40dbdf2854a1248b778a04
sriramgoteti-1 <gotetisriram2268@gmail.com>
2022-12-21 15:16:49 +0530

    Third commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 
 import Header from "./components/header/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
-import React from 'react'
 
 function App() {
   var React= require('react')
